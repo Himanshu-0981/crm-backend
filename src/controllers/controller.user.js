6d822831bd280ccee45e485cce70fb40fcdda61b
Himanshu <kumarhimanshu2219@gmail.com>
2023-12-20 15:16:06 +0530

    user login completed

--- src/controllers/controller.user.js
+++ src/controllers/controller.user.js
@@ -1,10 +1,12 @@
+const jwt = require("jsonwebtoken");
+
 const userModel = require("../models/model.user");
 const sendError = require("../utils/util.error");
 const hash = require("../utils/util.hash");
 const sendResponse = require("../utils/util.response");
-const jwt = require("jsonwebtoken");
-// const sendVerifyMail = require("../utils/util.sendVerifyMail");
 const jwtSecretKey = require("../config/env_config").JWT_SECRET_KEY;
+const compare = require("../utils/util.compare");
+// const sendVerifyMail = require("../utils/util.sendVerifyMail");
 
 const handleUserSignUp = async (req, res) => {
   const { name, email, phone, address, password, confirmPassword } = req.body;
@@ -60,4 +62,43 @@ const handleUserSignUp = async (req, res) => {
   }
 };
 
-module.exports = { handleUserSignUp };
+const handleUserLogin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (email && password) {
+      const user = await userModel.findOne({ email });
+
+      if (user) {
+        // decrypting and comparing the password
+        const isMatch = await compare(password, user?.password);
+        if (isMatch) {
+          const token = jwt.sign({ userId: user?._id }, jwtSecretKey, {
+            expiresIn: "5d",
+          });
+
+          sendResponse(res, 200, true, "user login successfully", {
+            token,
+            user: {
+              email,
+            },
+          });
+        } else {
+          sendResponse(
+            res,
+            401,
+            false,
+            "Email or password doesn't match. Try again"
+          );
+        }
+      } else {
+        sendResponse(res, 404, false, "user not found please signup");
+      }
+    } else {
+      sendResponse(res, 400, false, "All field is required");
+    }
+  } catch (err) {
+    sendError(res, err);
+  }
+};
+
+module.exports = { handleUserSignUp, handleUserLogin };
--- src/routers/router.user.js
+++ src/routers/router.user.js
@@ -1,8 +1,12 @@
 const app = require("express");
-const { handleUserSignUp } = require("../controllers/controller.user");
+const {
+  handleUserSignUp,
+  handleUserLogin,
+} = require("../controllers/controller.user");
 
 const router = app.Router();
 
 router.post("/signup", handleUserSignUp);
+router.post("/login", handleUserLogin);
 
 module.exports = router;
